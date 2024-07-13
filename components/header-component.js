2389c0c9302c7c650a47f847dd14c6db9f7673b8
lvincent <leonelexequielvincent@gmail.com>
2024-07-13 18:49:23 -0300

    Estructura basica de Bairon

---
+++ components/header-component.js
@@ -0,0 +1,31 @@
+Vue.component('header-component', {
+  template: `
+
+    <div class="container text-center">
+      <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" @click="closeNav">&times;</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+      </div>
+      <div class="row">
+        <div class="col">
+          LOGO
+        </div>
+        
+        <div class="col">
+          <span style="font-size:30px;cursor:pointer" @click="openNav">&#9776;</span>
+        </div>
+      </div>
+    </div>
+  `,
+  methods: {
+    openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+    },
+    closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+    }
+  }
+});
