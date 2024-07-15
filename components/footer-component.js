85b230476ed9c6025293e28d1817994f6d20eeb5
unknown <matiasvincent2002@gmail.com>
2024-07-15 10:26:11 -0300

    agregando marcas

--- components/footer-component.js
+++ components/footer-component.js
@@ -1,23 +1,24 @@
-Vue.component('footer-component',{
+Vue.component('footer-component', {
     template: `
-    <footer>
+      <footer>
         <div>
-            <ul>
+          <ul>
             <li>QUIENES SOMOS</li>
             <li>SERVICIOS</li>
             <li>CASOS DE ÉXITO</li>
             <li>CONTACTO</li>
-            </ul>
-            <div class="icon">
-                <img src="./images/twitter.svg">
-                <img src="./images/facebook.svg">
-                <img src="./images/youtube.svg">
-                <img src="./images/linkedin.svg">
-            </div>
+          </ul>
+          <div class="icon">
+            <img src="./images/twitter.svg" alt="Twitter">
+            <img src="./images/facebook.svg" alt="Facebook">
+            <img src="./images/youtube.svg" alt="YouTube">
+            <img src="./images/linkedin.svg" alt="LinkedIn">
+          </div>
         </div>
         <div>
-         <p id="CR">Copyright 2024 - Media Bairon Content<p>
+          <p id="CR">Copyright 2024 - Media Bairon Content</p>
         </div>
-    </footer>
+      </footer>
     `
-})
+  });
+  
---
+++ components/marcas-component.js
@@ -0,0 +1,55 @@
+Vue.component('marcas-component', {
+  template: `
+    <div class="marcas-component">
+      <h1 class="text-center font-weight-bold">ACOMPAÑAMOS</h1>
+    <p class="text-center mx-auto my-5" style="width: 80%; font-size: 14px;  margin: 0 auto;
+     padding: 3rem 0;">
+      Los proyectos exitosos son el resultado de la suma de pequeños detalles realizados con dedicación, y a su vez, el factor humano y la empatía con cada uno de nuestros clientes. Estos son algunos que nos eligieron y nos siguen eligiendo:
+    </p>
+      <div class="marcas-container">
+      <div class="grid-item">
+        <img src="../utils/img/BAIRON - Logos Web 02_07 - Aloha Gris 1.svg" alt="Imagen 1">
+      </div>
+      <div class="grid-item">
+        <img src="../utils/img/BAIRON - Logos Web 02_07 - Aurora Alfonso Gris 1.svg" alt="Imagen 2">
+      </div>
+      <div class="grid-item">
+        <img src="../utils/img/BAIRON - Logos Web 02_07 - Cala Gris 1.svg" alt="Imagen 3">
+      </div>
+      <div class="grid-item">
+        <img src="../utils/img/BAIRON - Logos Web 02_07 - Campari Gris 1.svg" alt="Imagen 4">
+      </div>
+      <div class="grid-item">
+        <img src="../utils/img/BAIRON - Logos Web 02_07 - Chandon Gris 1.svg" alt="Imagen 5">
+      </div>
+      <div class="grid-item">
+        <img src="../utils/img/BAIRON - Logos Web 02_07 - Corona Gris 1.svg" alt="Imagen 6">
+      </div>
+      <div class="grid-item">
+        <img src="../utils/img/BAIRON - Logos Web 02_07 - Freddo Gris 1.svg" alt="Imagen 7">
+      </div>
+      <div class="grid-item">
+        <img src="../utils/img/BAIRON - Logos Web 02_07 - Ginebra Gris 1.svg" alt="Imagen 8">
+      </div>
+      <div class="grid-item">
+        <img src="../utils/img/BAIRON - Logos Web 02_07 - Johnson & Johnson Gris 2 Líneas 1.svg" alt="Imagen 9">
+      </div>
+      <div class="grid-item">
+        <img src="../utils/img/BAIRON - Logos Web 02_07 - Mary Kay Gris 1.svg" alt="Imagen 10">
+      </div>
+      <div class="grid-item">
+        <img src="../utils/img/BAIRON - Logos Web 02_07 - Mas Colonia Gris 1.svg" alt="Imagen 11">
+      </div>
+      <div class="grid-item">
+        <img src="../utils/img/BAIRON - Logos Web 02_07 - Sieger Gris 1.svg" alt="Imagen 12">
+      </div>
+      <div class="grid-item">
+        <img src="../utils/img/BAIRON - Logos Web 02_07 - Sofitel Gris 1.svg" alt="Imagen 13">
+      </div>
+      <div class="grid-item">
+        <img src="../utils/img/BAIRON - Logos Web 02_07 - Sweet Gris 1.svg" alt="Imagen 14">
+      </div>
+        </div>
+    </div>
+  `
+});
