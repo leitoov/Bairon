225664ea26658c5125b6393ca668a26fb464b361
unknown <matiasvincent2002@gmail.com>
2024-07-14 21:26:16 -0300

    agregando footer, form, preForm

---
+++ components/footer-component.js
@@ -0,0 +1,23 @@
+Vue.component('footer-component',{
+    template: `
+    <footer>
+        <div>
+            <ul>
+            <li>QUIENES SOMOS</li>
+            <li>SERVICIOS</li>
+            <li>CASOS DE ÉXITO</li>
+            <li>CONTACTO</li>
+            </ul>
+            <div class="icon">
+                <img src="../twiter.svg">
+                <img src="../facebook.svg">
+                <img src="../youtube.svg">
+                <img src="../linkedin.svg">
+            </div>
+        </div>
+        <div>
+         <p id="CR">Copyright 2024 - Media Bairon Content<p>
+        </div>
+    </footer>
+    `
+})
