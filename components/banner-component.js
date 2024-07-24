93144c74209fe0e812e497c004ba9a4328b0989e
unknown <matiasvincent2002@gmail.com>
2024-07-24 00:29:20 -0300

    agregando banner  MAS ANIMATE

--- components/banner-component.js
+++ components/banner-component.js
@@ -1,45 +1,55 @@
 Vue.component('banner-component', {
-  data(){
+  data() {
     return {
-      banners : [{
-        title:"+150%",
-        content:"Aumento de conversiones en 6 meses",
+      banners: [{
+        title: "+150%",
+        content: "Aumento de conversiones en 6 meses",
         color: "#A4C8BA",
-        imageUrl:"Antartida.svg",
-        logoImg:"AntartidaLogo.svg"
+        imageUrl: "Antartida.svg",
+        logoImg: "AntartidaLogo.svg"
+      }, {
+        title: "+150%",
+        content: "Aumento de conversiones en 6 meses",
+        color: "red",
+        imageUrl: "Antartida.svg",
+        logoImg: "AntartidaLogo.svg"
       }],
-      currentItem:0
+      currentItem: 0
     }
   },
- 
   template: `
-    <div v-for="(banner, index) in banners" :key="banner.title"
-    class="banner" :style="{ backgroundColor: banner.color }">
-      <div class="banner-logo">          
-      <img :src="getImageUrl(banner.logoImg)" :alt="banner.title">
-      </div>
-      <div class="titleContainer">
-        <h1>{{ banner.title }}</h1>
-        <p>{{ banner.content }}</p>
-      </div>
-      <div class="banner-img-container">   
-        <div class="banner-img">
-          <img :src="getImageUrl(banner.imageUrl)" :alt="banner.title">
+    <div v-if="banners.length > 0">
+      <div v-for="(banner, index) in banners" :key="banner.title"
+        class="banner" :class="{ active: index === currentItem }"
+        :style="{ backgroundColor: banner.color }">
+        <div class="banner-logo">          
+          <img :src="getImageUrl(banner.logoImg)" :alt="banner.title">
         </div>
-        <div class="banner-img">
-          <img :src="getImageUrl(banner.imageUrl)" :alt="banner.title">
+        <div class="titleContainer">
+          <h1>{{ banner.title }}</h1>
+          <p>{{ banner.content }}</p>
         </div>
-        <div class="banner-img">
-          <img :src="getImageUrl(banner.imageUrl)" :alt="banner.title">
-        </div>
-      </div>
-      <button>
-      </button>
+        <div class="banner-img-container">   
+          <div class="banner-img">
+            <img :src="getImageUrl(banner.imageUrl)" :alt="banner.title">
+          </div>
+          <div class="banner-img">
+            <img :src="getImageUrl(banner.imageUrl)" :alt="banner.title">
+          </div>
+          <div class="banner-img">
+            <img :src="getImageUrl(banner.imageUrl)" :alt="banner.title">
+          </div>
+        </div><button @click="nextBanner" ></button>
+      </div> 
+     
     </div>
   `,
   methods: {
     getImageUrl(img) {
       return "./images/" + img;
+    },
+    nextBanner() {
+      this.currentItem = (this.currentItem + 1) % this.banners.length;
     }
   }
 });
