408f5044e8e77911b5456a8a2ba1935fb390c0cf
lvincent <leonelexequielvincent@gmail.com>
2024-08-11 13:39:39 -0300

    Ajustes de estilos y header

--- components/header-component.js
+++ components/header-component.js
@@ -3,14 +3,14 @@ Vue.component('header-component', {
     <div class="container text-center">
       <div id="mySidenav" class="sidenav">
         <a href="javascript:void(0)" class="closebtn" @click="closeNav">&times;</a>
-        <a href="#quienes-somos"  @click="closeNav">QUIENES SOMOS</a>
+        <a href="#quienes-somos"  @click="closeNav" style="margin-top: 15px;">QUIENES SOMOS</a>
         <a href="#servicios"  @click="closeNav">SERVICIOS</a>
         <a href="#caso-exito"  @click="closeNav">CASOS DE ÉXITO</a>
         <a href="#contacto"  @click="closeNav" style="margin-bottom: 10px;">CONTACTO</a>
       </div>
       <div class="row">
         <div class="col" style="align-items: end; display: flex; margin: auto;">
-          <img src="./images/Logo.svg"/>
+          <img src="./images/Logo.svg" style="width:10%;">
         </div>
         
         <div class="col" style="justify-content: end;display: flex;margin: auto; padding-right: 50px;">
@@ -25,7 +25,7 @@ Vue.component('header-component', {
   methods: {
     openNav() {
       document.getElementById("mySidenav").style.width = "270px";
-      document.getElementById("mySidenav").style.height = "auto";
+      document.getElementById("mySidenav").style.height = "300px";
       document.getElementById("mySidenav").style.boxShadow = "-5px 7px 13px 4px rgba(0, 0, 0, 1)";
       document.getElementById("overlay").style.display = "block"; // Muestra el overlay
     },
