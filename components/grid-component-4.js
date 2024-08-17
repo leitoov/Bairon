30eb6e49673681e5b92999301a7a235575391268
unknown <matiasvincent2002@gmail.com>
2024-08-17 10:51:55 -0300

    agregando landign (falta arreglar info y detalles de css

---
+++ components/grid-component-4.js
@@ -0,0 +1,24 @@
+Vue.component('grid-component-4', {
+    props: {
+      images: {
+        type: Array,
+        required: true,
+        default: () => ([
+          { src: "images/img1ContainerGrid.svg", alt: "Image 1" },
+          { src: "images/img3ContainerGrid.svg", alt: "Image 2" },
+          { src: "images/img2ContainerGrid.svg", alt: "Image 3" },
+          { src: "images/img4ContainerGrid.svg", alt: "Image 4" }
+        ])
+      }
+    },
+    template: `
+      <div class="row">
+        <div class="grid-container">
+          <div v-for="(image, index) in images" :key="index" :class="'grid-item item-' + (index + 1)">
+            <img :src="image.src" :alt="image.alt">
+          </div>
+        </div>
+      </div>
+    `
+  });
+  
--- components/grid-component.js
+++ components/grid-component.js
@@ -1,24 +1,24 @@
 Vue.component('grid-component', {
-    props: {
-      images: {
-        type: Array,
-        required: true,
-        default: () => ([
-          { src: "images/img1ContainerGrid.svg", alt: "Image 1" },
-          { src: "images/img3ContainerGrid.svg", alt: "Image 2" },
-          { src: "images/img2ContainerGrid.svg", alt: "Image 3" },
-          { src: "images/img4ContainerGrid.svg", alt: "Image 4" }
-        ])
-      }
-    },
-    template: `
-      <div class="row fade-in-bottom">
-        <div class="grid-container">
-          <div v-for="(image, index) in images" :key="index" :class="'grid-item item-' + (index + 1)">
-            <img :src="image.src" :alt="image.alt">
-          </div>
-        </div>
+  props: {
+    content: {
+      type: Object,
+      required: true,
+      default: () => ({
+        title: "Generación de contenido",
+        subtitle: "Inspiración en acción, creatividad en movimiento",
+        imageUrl: "./images/Ellipse 7.svg"
+      })
+    }
+  },
+  template: `
+    <div class="row fade-in-bottom">
+      <div class="col" style="display: flex; flex-direction: column;">
+        <p>{{ content.title }}</p>
+        <p style="width:90%;font-weight: 700; font-size: 60px; line-height: 72px;">{{ content.subtitle }}</p>
       </div>
-    `
-  });
-  
+      <div class="col" style="display:flex;justify-content: flex-end;">
+        <img :src="content.imageUrl" alt="Illustration">
+      </div>
+    </div>
+  `
+});
